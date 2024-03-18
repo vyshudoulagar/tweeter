62bf42573d09ee917d0cb34d809ee9ca5c201c67
WishNavy <du.vyshu@gmail.com>
2024-03-17 20:32:50 -0700

    Add an example tweet and css for it

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,11 +3,12 @@ $(document).ready(function() {
     textarea.on('input', function() {   //using the event handler input 
         const textLength = $(this).val().length; //assigning the length of input to textLength
         const remaining = 140 - textLength;
-        $('.counter').text(remaining);  //updating counter value as the textLength changes
+        const counter = $('.counter');
+        counter.text(remaining);  //updating counter value as the textLength changes
         if(remaining < 0) {
-            $('.counter').addClass('negative'); //add class negative to change counter value color to red when negative
+            counter.addClass('negative'); //add class negative to change counter value color to red when negative
         } else {
-            $('.counter').removeClass('negative'); //remove class negative when counter value is positive
+            counter.removeClass('negative'); //remove class negative when counter value is positive
         }
     });
 });

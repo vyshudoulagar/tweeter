119b033365451400e23b74c81659834187c3c112
WishNavy <du.vyshu@gmail.com>
2024-03-19 15:53:45 -0700

    fetch data using AJAX get method

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,31 +3,31 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-const data = [
-    {
-        "user": {
-            "name": "Newton",
-            "avatars": "https://i.imgur.com/73hZDYK.png"
-            ,
-            "handle": "@SirIsaac"
-        },
-        "content": {
-            "text": "If I have seen further it is by standing on the shoulders of giants"
-        },
-        "created_at": 1461116232227
-    },
-    {
-        "user": {
-            "name": "Descartes",
-            "avatars": "https://i.imgur.com/nlhLi3I.png",
-            "handle": "@rd"
-        },
-        "content": {
-            "text": "Je pense , donc je suis"
-        },
-        "created_at": 1461113959088
-    }
-];
+// const data = [
+//     {
+//         "user": {
+//             "name": "Newton",
+//             "avatars": "https://i.imgur.com/73hZDYK.png"
+//             ,
+//             "handle": "@SirIsaac"
+//         },
+//         "content": {
+//             "text": "If I have seen further it is by standing on the shoulders of giants"
+//         },
+//         "created_at": 1461116232227
+//     },
+//     {
+//         "user": {
+//             "name": "Descartes",
+//             "avatars": "https://i.imgur.com/nlhLi3I.png",
+//             "handle": "@rd"
+//         },
+//         "content": {
+//             "text": "Je pense , donc je suis"
+//         },
+//         "created_at": 1461113959088
+//     }
+// ];
 
 $(document).ready(function() {
     const createTweetElement = (tweetObject) => {
@@ -43,7 +43,7 @@ $(document).ready(function() {
         </header>
         <p class="content">${tweetObject.content.text}</p>
         <footer class="footer">
-            <span class="created-on">${tweetObject.created_at}</span>
+            <span class="created-at">${timeago.format(tweetObject.created_at)}</span>
             <span class="icons">
                 <i class="fa-solid fa-flag"></i>&nbsp;&nbsp;&nbsp;
                 <i class="fa fa-retweet"></i>&nbsp;&nbsp;&nbsp;
@@ -64,7 +64,7 @@ $(document).ready(function() {
         }
     };
 
-    renderTweets(data);
+    // renderTweets(data);
 
 
 $('#tweet-form').on('submit', function(event) {
@@ -87,6 +87,21 @@ $('#tweet-form').on('submit', function(event) {
             console.log(xhr.responseText);
         }
     })
-})
+});
+
+const loadTweets = function() {
+    //Make AJAX GET request
+    $.ajax('/tweets/', {
+        method: 'GET',
+        success: function(tweets) {
+            renderTweets(tweets);
+        },
+        error: function(xhr, status, error) {
+            console.log(error);
+        }
+    });
+};
+
+loadTweets();
 
 });

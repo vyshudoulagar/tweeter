f80d2cebb0b82f5d2ed9f65b5835497ab54b7169
WishNavy <du.vyshu@gmail.com>
2024-03-20 11:33:55 -0700

    Use .text method to avoid XSS

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,105 +3,25 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-// const data = [
-//     {
-//         "user": {
-//             "name": "Newton",
-//             "avatars": "https://i.imgur.com/73hZDYK.png"
-//             ,
-//             "handle": "@SirIsaac"
-//         },
-//         "content": {
-//             "text": "If I have seen further it is by standing on the shoulders of giants"
-//         },
-//         "created_at": 1461116232227
-//     },
-//     {
-//         "user": {
-//             "name": "Descartes",
-//             "avatars": "https://i.imgur.com/nlhLi3I.png",
-//             "handle": "@rd"
-//         },
-//         "content": {
-//             "text": "Je pense , donc je suis"
-//         },
-//         "created_at": 1461113959088
-//     }
-// ];
 
-$(document).ready(function() {
-    const createTweetElement = (tweetObject) => {
-        const $tweet = $(`<article class="tweet">
-        <header class="header">
-            <div class="name">
-                <img src="${tweetObject.user.avatars}" alt="User Avatar">
-                <span>${tweetObject.user.name}</span>
-            </div>
-            <div>
-                <span class="handle">${tweetObject.user.handle}</span>
-            </div>
-        </header>
-        <p class="content">${tweetObject.content.text}</p>
-        <footer class="footer">
-            <span class="created-at">${timeago.format(tweetObject.created_at)}</span>
-            <span class="icons">
-                <i class="fa-solid fa-flag"></i>&nbsp;&nbsp;&nbsp;
-                <i class="fa fa-retweet"></i>&nbsp;&nbsp;&nbsp;
-                <i class="fa fa-heart"></i>&nbsp;&nbsp;&nbsp;
-            </span>
-        </footer>
-    </article>`);
-        return $tweet;
-    };
+$(() => { //start after the document is ready
+    $('#tweet-form').on('submit', postTweet);
+});
 
-    const renderTweets = function(tweets) {
-        // loops through tweets
-        for (const tweet of tweets) {
-            // calls createTweetElement for each tweet
-            const $tweet = createTweetElement(tweet);
-            // takes return value and appends it to the tweets container
-            $('.tweets-container').prepend($tweet);
-        }
-    };
+//Functions
+const postTweet = (event) => {
+    //Prevents default from submission
+    event.preventDefault();
 
-    const resetTweetForm = () => {
-        $('#tweet-form')[0].reset();
-    };
-
-    const resetCharCounter = () => {
-        const maxChars = 140;
-        $('.counter').text(maxChars);
-    };
-
-    const isTweetValid = () => {
-        const textCount = $('#tweet-text');
-        const textLength = (textCount.val().trim().length);
-        if (textLength === 0) {
-            alert("Your tweet cannot be empty");
-            return;
-        }
-        if (textLength >= 140) {
-            alert("Your tweet cannot exceed 140 characters");
-            return;
-        }
-    };
-
-    $('#tweet-form').on('submit', function(event) {
-        //Prevents default from submission
-        event.preventDefault();
-
-        //Validating tweet form
-        isTweetValid();
+    //Validating tweet form
+    if (isTweetValid()) {
 
         //Serialise form data
-        const formData = $(this).serialize();
+        const formData = $('#tweet-form').serialize();
 
         //Make AJAX POST request
-        $.post({
-            url: '/tweets/',
-            data: formData
-        })
-            .then(function(response) {
+        $.post({ url: '/tweets/', data: formData })
+            .then((response) => {
                 //Handle successful response
                 $('#response').html(response);
                 //Reset the tweet form
@@ -111,23 +31,84 @@ $(document).ready(function() {
                 //call the function to load tweets
                 loadTweets();
             })
-            .catch(function(xhr, status, error) {
+            .catch((xhr, status, error) => {
                 //Handle error
                 alert("tweet could not be posted");
                 console.log(xhr.responseText);
             });
-    });
+    }
+};
 
+const loadTweets = () => {
+    //clear tweet container
+    $('.tweets-container').empty();
+    //Make AJAX GET request
+    $.get('/tweets/')
+        .then(function(tweets) {
+            renderTweets(tweets);
+        })
+        .catch(function(xhr, status, error) {
+            console.log(error);
+        });
+};
 
-    const loadTweets = function() {
-        //Make AJAX GET request
-        $.ajax('/tweets/', { method: 'GET' })
-            .then(function(tweets) {
-                renderTweets(tweets);
-            })
-            .catch(function(xhr, status, error) {
-                console.log(error);
-            });
-    };
+const renderTweets = (tweets) => {
+    // loops through tweets
+    for (const tweet of tweets) {
+        // calls createTweetElement for each tweet
+        const $tweet = createTweetElement(tweet);
+        // takes return value and appends it to the tweets container
+        $('.tweets-container').prepend($tweet);
+    }
+};
 
-});
+const createTweetElement = (tweetObject) => {
+    //Using .text() method to avoid XSS
+    const $tweet = $('<article>').addClass('tweet');
+    const $header = $('<header>').addClass('header');
+    const $name = $('<div>').addClass('name');
+    $name.append($('<img>').attr('src', tweetObject.user.avatars).attr('alt', 'User Avatar'));
+    $name.append($('<span>').text(tweetObject.user.name));
+    $header.append($name);
+    $header.append($('<div>').append($('<span>').addClass('handle').text(tweetObject.user.handle)));
+    $tweet.append($header);
+    
+    $tweet.append($('<p>').addClass('content').text(tweetObject.content.text));
+
+    const $footer = $('<footer>').addClass('footer');
+    $footer.append($('<span>').addClass('created-at').text(timeago.format(tweetObject.created_at)));
+    const $icons = $('<span>').addClass('icons');
+    $icons.append($('<i>').addClass('fa-solid fa-flag'));
+    $icons.append('&nbsp;&nbsp;&nbsp;');
+    $icons.append($('<i>').addClass('fa fa-retweet'));
+    $icons.append('&nbsp;&nbsp;&nbsp;');
+    $icons.append($('<i>').addClass('fa fa-heart'));
+    $footer.append($icons);
+    $tweet.append($footer);
+
+    return $tweet;
+};
+
+const resetTweetForm = () => {
+    $('#tweet-form')[0].reset();
+};
+
+const resetCharCounter = () => {
+    const maxChars = 140;
+    $('.counter').text(maxChars);
+};
+
+const isTweetValid = () => {
+    const textCount = $('#tweet-text');
+    const textLength = (textCount.val().trim().length);
+    if (textLength === 0) {
+        alert("Your tweet cannot be empty");
+        return false;
+    }
+    if (textLength >= 140) {
+        alert("Your tweet cannot exceed 140 characters");
+        return false;
+    }
+    return true;
+};
+
